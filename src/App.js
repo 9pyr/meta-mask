32f4e982c057a6327e460599c7e8b208d6e7c598
Peeraphon Singla <ext.peeraphon.sin@truedigital.com>
2021-12-23 19:36:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useLayoutEffect, useState } from 'react'
+import Web3 from 'web3'
+import './App.css'
+
+function App() {
+  const [accountId, setAccountId] = useState()
+  const [balance, setBalance] = useState(0)
+
+  const handlerConnectWallet = async () => {
+    const { ethereum } = window
+
+    if (!ethereum) return
+
+    const [account] = await ethereum.request({ method: 'eth_requestAccounts' })
+
+    setAccountId(account)
+  }
+
+  const getAccountInfo = async () => {
+    const { ethereum } = window
+
+    if (!ethereum) return
+
+    console.log('🔥 connect wallet')
+    const [account] = await ethereum.request({ method: 'eth_accounts' })
+
+    setAccountId(account)
+
+    if (accountId) {
+      const web3 = new Web3(ethereum)
+      const balance = await web3.eth.getBalance(account)
+
+      setBalance(balance)
+    }
+  }
+
+  useLayoutEffect(() => {
+    getAccountInfo()
+  }, [])
+
+  return (
+    <div className='App'>
+      {!accountId && (
+        <div>
+          <div className='text'>you not connect wallet</div>
+          <div>
+            <button onClick={handlerConnectWallet}>connect wallet</button>
+          </div>
+        </div>
+      )}
+      {accountId && (
+        <div className='card'>
+          <div>Address: {accountId}</div>
+          <div>Balance: {balance} Ether</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
